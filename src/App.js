5a39d9f595078664dd043254a68ae2e150cebde1
Sourav01112 <chaudharysourav997@gmail.com>
2023-07-07 04:27:35 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { MultiStepProgressBar } from "./components/MultiStepProgressBar";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
@@ -15,42 +15,59 @@ function App() {
 
   const prevButton = () => {
     if (index > 1) {
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
     }
-  }
+  };
+
+  // const nextButton = () => {
+  //   if (index - 3) {
+  //     setIndex((prevIndex) => prevIndex + 1);
+  //   } else {
+  //     // clear the form on submit
+  //     setPagesAnswers({});
+  //     setSubmitted(true);
+  //     if (Object.keys(pagesAnswers).length === 0) {
+  //       alert("Please provide answers before proceeding to the next page.");
+  //       return;
+  //     }
+  //   }
+  // };
 
   const nextButton = () => {
     if (index - 3) {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     } else {
-      // clear the form on submit
+      // Check if pagesAnswers is not empty before proceeding
+      if (Object.keys(pagesAnswers).length === 0) {
+        alert("Please provide answers before submitting the form.");
+        return;
+      }
+
+      // Clear the form on submit
       setPagesAnswers({});
       setSubmitted(true);
     }
-  }
+  };
 
   const onPageAnswerUpdate = (step, answersObj) => {
-    setPagesAnswers({...pagesAnswers, [step]: answersObj});
-  }
+    setPagesAnswers({ ...pagesAnswers, [step]: answersObj });
+  };
 
   const handleStart = () => {
     setIndex(1);
     setSubmitted(false);
-  }
-
+  };
+  console.log(pagesAnswers);
   return (
     <div className="App">
       <Container className="h-100">
         <Row className="m-5">
           <Col className="align-self-center">
-            <MultiStepProgressBar
-              step={index}
-              />
+            <MultiStepProgressBar step={index} />
           </Col>
         </Row>
         <Row>
-          {
-            submitted ?
+          {submitted ? (
             <Card>
               <Card.Body>
                 <p>Your answers have been submitted!</p>
@@ -58,22 +75,27 @@ function App() {
               <Card.Footer>
                 <Button onClick={handleStart}>Start Over</Button>
               </Card.Footer>
-            </Card> :
-          <Card>
-            <Card.Body>
-              <MultiStepForm
-                list={questions}
-                step={index}
-                onPageUpdate={onPageAnswerUpdate}
-                pagesAnswers={pagesAnswers}
+            </Card>
+          ) : (
+            <Card>
+              <Card.Body>
+                <MultiStepForm
+                  list={questions}
+                  step={index}
+                  onPageUpdate={onPageAnswerUpdate}
+                  pagesAnswers={pagesAnswers}
                 />
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-between">
-              <Button onClick={prevButton} disabled={index == 1}>Previous</Button>
-              <Button onClick={nextButton}>{index == totalPagesCount ? 'Submit' : 'Next'}</Button>
-            </Card.Footer>
-          </Card>
-        }
+              </Card.Body>
+              <Card.Footer className="d-flex justify-content-between">
+                <Button onClick={prevButton} disabled={index == 1}>
+                  Previous
+                </Button>
+                <Button onClick={nextButton}>
+                  {index == totalPagesCount ? "Submit" : "Next"}
+                </Button>
+              </Card.Footer>
+            </Card>
+          )}
         </Row>
       </Container>
     </div>
